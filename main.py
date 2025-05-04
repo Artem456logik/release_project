fe786687a07813d67c82ec6727d6a01752feb8b2
art <art@gmail.com>
2025-05-04 12:26:17 +0300

    the end

--- main.py
+++ main.py
@@ -9,6 +9,22 @@ FPS = 60
 Score = 0
 Life = 1
 
+pygame.init()
+
+cursor_img = pygame.transform.scale(
+    pygame.image.load("pricel.png"),
+    (75,75)
+)
+
+
+pygame.mouse.set_visible(False)
+cursor_img_rect = cursor_img.get_rect()
+
+bloodstn = pygame.transform.scale(
+    pygame.image.load("bloodstn-removebg-preview.png"),
+    (50,50)
+)
+
 window = pygame.display.set_mode(SIZE)
 background = pygame.transform.scale(
     pygame.image.load("background.jpg"),
@@ -16,6 +32,10 @@ background = pygame.transform.scale(
 gameoverbg = pygame.transform.scale(
     pygame.image.load("endgame.png"),
     SIZE)
+victorybg = pygame.transform.scale(
+    pygame.image.load("Win.jpg"),
+    SIZE
+)
 pygame.display.set_caption("ШУТЕР 2Д")
 clock = pygame.time.Clock()
 
@@ -39,11 +59,18 @@ class GameSprite(pygame.sprite.Sprite):
 class Enemy(GameSprite):
     def __init__(self, filename: str, size: tuple[int, int], coords: tuple[int, int], speed: int):
         super().__init__(filename, size, coords, speed )
-        self.timer = randint(5,8)
+        self.timer = randint(2,3)
     def update(self):
         self.timer -= 1
 
+bombs = pygame.sprite.Group()
+
 enemy = Enemy("enemy.png", (70,50), (random.randint(50, WIDTH-50), 125), random.randint(1,3))
+for i in range(20):
+    new_bomb = GameSprite("bombC4.png", (70,50), (random.randint(100, WIDTH-50), random.randint(25,495)), 0)
+    while pygame.sprite.spritecollideany(new_bomb,bombs) and pygame.sprite.collide_rect(new_bomb,enemy):
+        new_bomb = GameSprite("bombC4.png", (70,50), (random.randint(100, WIDTH-50), random.randint(0,495)), 0)
+    bombs.add(new_bomb)
 
 
 game = True
@@ -62,14 +89,22 @@ while game:
                 Score += 1
     if not finish and not restart:
         window.blit(background, (0,0))
+        cursor_img_rect.center = pygame.mouse.get_pos()
+        window.blit(cursor_img, cursor_img_rect) 
         enemy.reset(window)
+        bombs.draw(window)
         newtime = time.time()
-        score_text = medium_font.render("Збито" + str(Score), True, (255,255,255))
-        timer_text = medium_font.render("Лишилося:" + str(enemy.timer), True, (255,0,0))
+        score_text = medium_font.render("Збито " + str(Score), True, (255,255,255))
+        timer_text = medium_font.render("Лишилося: " + str(enemy.timer), True, (255,0,0))
+        lifes_text = medium_font.render("Життя " + str(Life), True, (255,255,0))
+        victory_text = medium_font.render("Перемога", True, (247,255,0))
         window.blit(score_text, (25,50))
         window.blit(timer_text, enemy.rect)
+        window.blit(lifes_text, (25,25))
         if Score >= 10:
             finish = True
+            window.blit(victorybg, (0,0))
+            window.blit(victory_text, (500,25))
         if newtime - currenttime >= 1:
             enemy.update()
             timer_text = medium_font.render("Лишилося:" + str(enemy.timer), True, (255,255,255))
